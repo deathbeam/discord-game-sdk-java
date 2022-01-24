957be7db5af2a28a1cd1deb87c1dbdf188226943
Tomas Slusny <slusnucky@gmail.com>
2022-01-24 21:40:47 +0100

    Add UserManager getUser extension
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- discord-game-sdk/src/main/java/io/github/deathbeam/discordgamesdk/extensions/DiscordUserManagerExtensions.java
+++ discord-game-sdk/src/main/java/io/github/deathbeam/discordgamesdk/extensions/DiscordUserManagerExtensions.java
@@ -2,6 +2,7 @@ package io.github.deathbeam.discordgamesdk.extensions;
 
 import io.github.deathbeam.discordgamesdk.jna.DiscordUser;
 import io.github.deathbeam.discordgamesdk.jna.IDiscordUserManager;
+import java.util.concurrent.CompletableFuture;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
@@ -12,5 +13,12 @@ public class DiscordUserManagerExtensions
 		final DiscordUser discordUser = new DiscordUser();
 		userManager.get_current_user.apply(userManager, discordUser);
 		return discordUser;
+	}
+
+	public static CompletableFuture<DiscordUser> getUser(final IDiscordUserManager userManager, final long userId)
+	{
+		final CompletableFuture<DiscordUser> future = new CompletableFuture<>();
+		userManager.get_user.apply(userManager, userId, null, (callback_data, result, user) -> future.complete(user));
+		return future;
 	}
 }
